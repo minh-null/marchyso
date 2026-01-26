1fda29f4b23ffad31dfe5f5db39b97329e81e504
Minh <124509308+minh-null@users.noreply.github.com>
2026-01-26 22:49:35 +0700

    Added Wallpapers

--- install.sh
+++ install.sh
@@ -3,13 +3,19 @@
 username=$(whoami)
 wd=$(pwd)
 
-sudo pacman -S fastftech hyprland cava waybar wofi fastfetch
+sudo pacman -S fastftech hyprland cava waybar wofi fastfetch hyprpaper
 
 rm -rf /home/"$username"/.config/cava/
 rm -rf /home/"$username"/.config/hypr/
 rm -rf /home/"$username"/.config/waybar/
 rm -rf /home/"$username"/.config/fastfetch
 rm -rf /home/"$username"/.config/wofi
+
+mkdir /home/"$username"/Pictures/Wallpapers/
+cp "$wd"/wallpapers/6418406.jpg /home/"$username"/Pictures/Wallpapers/
+cp "$wd"/wallpapers/macos-monterey-wwdc-21-stock-dark-mode-5k-3840x2160-5585.jpg /home/"$username"/Pictures/Wallpapers/
+cp "$wd"/wallpapers/purple.jpeg /home/"$username"/Pictures/Wallpapers/
+cp "$wd"/wallpapers/win11-violet.jpg /home/"$username"/Pictures/Wallpapers/
 
 cp "$wd"/hypr /home/"$username"/.config
 cp "$wd"/cava /home/"$username"/.config
