3853e67dcc99737d555601bb153b04e0fbf4d1c1
Minh <124509308+minh-null@users.noreply.github.com>
2026-02-15 00:25:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 username=$(whoami)
 wd=$(pwd)
 
-sudo pacman -S fastftech hyprland cava waybar wofi fastfetch hyprpaper rofi-emoji papirus-icon-theme archlinux-xdg-menu
+sudo pacman -S --needed nodejs npm electron fastftech hyprland cava waybar wofi fastfetch hyprpaper rofi-emoji papirus-icon-theme archlinux-xdg-menu
 XDG_MENU_PREFIX=arch- kbuildsycoca6
 
 rm -rf /home/"$username"/.config/cava/
@@ -18,3 +18,9 @@ cp "$wd"/wallpapers/* /home/"$username"/Pictures/Wallpapers
 cp "$wd"/config* /home/"$username"/.config
 cp "$wd"/local-bin /home/"$username"/.local/bin
 cp "$wd"/.zshrc /home/"$username"
+
+git clone https://github.com/TechyTechster/ez-fm.git
+cd ez-fm
+
+./install.sh
+ez-fm
